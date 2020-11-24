162627f92b6064ef118b48cc407cf186567ce1ca
Raghu <raghu@gmail.com>
2020-11-23 21:17:25 -0500

    updated leapyear git push

---
+++ Java8Demo/src/main/java/com/abctech/service/Palindrome.java
@@ -0,0 +1,17 @@
+package com.abctech.service;
+
+public class Palindrome {
+
+    public static void main(String args[]) {
+
+        Integer year = 100;
+
+        if(year%4==0) {
+            System.out.println("leap year");
+        } else {
+            System.out.println(" not leap year");
+        }
+
+    }
+
+}
